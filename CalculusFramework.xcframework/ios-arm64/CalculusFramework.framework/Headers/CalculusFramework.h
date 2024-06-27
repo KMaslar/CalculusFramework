052ca33f9347a44519e690098cfd5e37ddbe84d9
Kostadina Geceska <kostadinag@nextsense.com>
2024-06-27 08:48:26 +0200

    init commit

---
+++ CalculusFramework.xcframework/ios-arm64/CalculusFramework.framework/Headers/CalculusFramework.h
@@ -0,0 +1,18 @@
+//
+//  CalculusFramework.h
+//  CalculusFramework
+//
+//  Created by Kostadina Gecevska on 27.6.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalculusFramework.
+FOUNDATION_EXPORT double CalculusFrameworkVersionNumber;
+
+//! Project version string for CalculusFramework.
+FOUNDATION_EXPORT const unsigned char CalculusFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculusFramework/PublicHeader.h>
+
+
